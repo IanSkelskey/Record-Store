8b691127976f54907438a21b2e3a713ed97eca5a
Ian Skelskey <ianskelskey@gmail.com>
2022-07-21 00:31:40 -0400

    Add employee and location

---
+++ Record-Store_Team_18_insert.sql
@@ -0,0 +1,19 @@
+-- Employees
+INSERT INTO EMPLOYEE VALUES
+  (1, "Ian Skelskey", 1);
+
+-- Locations
+INSERT INTO LOCATION VALUES
+  (8675309, "120 Jenny Lane, San Francisco, California 94102", "Tutone Music Shop", 1);
+
+-- Albums
+INSERT INTO ALBUM VALUES
+;
+
+-- Songs
+INSERT INTO SONG VALUES
+;
+
+-- Genres
+INSERT INTO GENRE VALUES
+;
