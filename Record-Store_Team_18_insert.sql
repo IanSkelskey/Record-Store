eb40fc6112333800606b15fe3fdaa7903146a161
Ian Skelskey <ianskelskey@gmail.com>
2022-07-21 12:25:59 -0400

    Add album In My Room by Jacob Collier

--- Record-Store_Team_18_insert.sql
+++ Record-Store_Team_18_insert.sql
@@ -8,12 +8,55 @@ INSERT INTO LOCATION VALUES
 
 -- Albums
 INSERT INTO ALBUM VALUES
-;
+	(1, "In My Room", "2016-07-01", 17.58),
+	(2, "Fleet Foxes", "2008-06-03", 13.98);
+
+-- Artists
+INSERT INTO ARTIST VALUES
+    (1, "Jacob Collier", "1994-08-02", "Jacob Collier");
 
 -- Songs
 INSERT INTO SONG VALUES
-;
+	(1, "Woke Up Today", 280, "Lyrics", 1),
+    (2, "In My Room", 289, "Lyrics", 1),
+    (3, "Hideaway", 412, "Lyrics", 1),
+    (4, "You And I", 260, "Lyrics", 1),
+    (5, "Down The Line", 398, "Lyrics", 1),
+    (6, "Now And Then I Think About You", 53, "Lyrics", 1),
+    (7, "Saviour", 368, "Lyrics", 1),
+    (8, "Hajanga", 362, "Lyrics", 1),
+    (9, "Flintstones", 190, "Lyrics", 1),
+    (10, "In The Real Early Morning", 369, "Lyrics", 1),
+    (11, "Don't You Know", 550, "Lyrics", 1);
+
+-- Song Features
+INSERT INTO SONG_FEATURE_LIST VALUES
+	(1, 1),
+    (1, 2),
+    (1, 3),
+    (1, 4),
+    (1, 5),
+    (1, 6),
+    (1, 7),
+    (1, 8),
+    (1, 9),
+    (1, 10),
+    (1, 11);
 
 -- Genres
 INSERT INTO GENRE VALUES
-;
+	("Jazz music is a broad style of music characterized by complex harmony, syncopated rhythms, and a heavy emphasis on improvisation.", "Jazz");
+
+-- Song Genre
+INSERT INTO SONG_GENRE_LIST VALUES
+	(1, "Jazz"),
+    (2, "Jazz"),
+    (3, "Jazz"),
+    (4, "Jazz"),
+    (5, "Jazz"),
+    (6, "Jazz"),
+    (7, "Jazz"),
+    (8, "Jazz"),
+    (9, "Jazz"),
+    (10, "Jazz"),
+    (11, "Jazz");
