38c57bb87891ded62828f3d266e4f8757c8b0779
Ian Skelskey <ianskelskey@gmail.com>
2022-07-25 22:29:41 -0400

    Autoformat create statements with workbench

--- Record-Store_Team_18_create.sql
+++ Record-Store_Team_18_create.sql
@@ -1,19 +1,18 @@
-CREATE TABLE Location
-(
-  LocationID INT NOT NULL AUTO_INCREMENT,
-  LocationName VARCHAR(45) NOT NULL,
-  PhoneNumber CHAR(10) NOT NULL,
-  Address VARCHAR(50) NOT NULL,
-  PRIMARY KEY (LocationID)
+CREATE TABLE Location (
+    LocationID INT NOT NULL AUTO_INCREMENT,
+    LocationName VARCHAR(45) NOT NULL,
+    PhoneNumber CHAR(10) NOT NULL,
+    Address VARCHAR(50) NOT NULL,
+    PRIMARY KEY (LocationID)
 );
 
-CREATE TABLE Employee
-(
-  EmployeeID INT NOT NULL AUTO_INCREMENT,
-  Name VARCHAR(30) NOT NULL,
-  LocationID INT NOT NULL,
-  PRIMARY KEY (EmployeeID),
-  FOREIGN KEY (LocationID) REFERENCES Location(LocationID)
+CREATE TABLE Employee (
+    EmployeeID INT NOT NULL AUTO_INCREMENT,
+    Name VARCHAR(30) NOT NULL,
+    LocationID INT NOT NULL,
+    PRIMARY KEY (EmployeeID),
+    FOREIGN KEY (LocationID)
+        REFERENCES Location (LocationID)
 );
 
 CREATE TABLE Album (
@@ -24,52 +23,52 @@ CREATE TABLE Album (
     PRIMARY KEY (AlbumID)
 );
 
-CREATE TABLE Song
-(
-  SongID INT NOT NULL AUTO_INCREMENT,
-  SongTitle VARCHAR(50) NOT NULL,
-  Duration Time NOT NULL,
-  Lyrics Text,
-  AlbumID INT NOT NULL,
-  PRIMARY KEY (SongID),
-  FOREIGN KEY (AlbumID) REFERENCES Album(AlbumID)
+CREATE TABLE Song (
+    SongID INT NOT NULL AUTO_INCREMENT,
+    SongTitle VARCHAR(50) NOT NULL,
+    Duration TIME NOT NULL,
+    Lyrics TEXT,
+    AlbumID INT NOT NULL,
+    PRIMARY KEY (SongID),
+    FOREIGN KEY (AlbumID)
+        REFERENCES Album (AlbumID)
 );
 
-CREATE TABLE Genre
-(
-  GenreName VARCHAR(25) NOT NULL,
-  Description MEDIUMTEXT NOT NULL,
-  PRIMARY KEY (GenreName)
+CREATE TABLE Genre (
+    GenreName VARCHAR(25) NOT NULL,
+    Description MEDIUMTEXT NOT NULL,
+    PRIMARY KEY (GenreName)
 );
 
-CREATE TABLE SongGenreList
-(
-  GenreName VARCHAR(25) NOT NULL,
-  SongID INT NOT NULL,
-  PRIMARY KEY (GenreName, SongID),
-  FOREIGN KEY (GenreName) REFERENCES Genre(GenreName),
-  FOREIGN KEY (SongID) REFERENCES Song(SongID)
+CREATE TABLE SongGenreList (
+    GenreName VARCHAR(25) NOT NULL,
+    SongID INT NOT NULL,
+    PRIMARY KEY (GenreName , SongID),
+    FOREIGN KEY (GenreName)
+        REFERENCES Genre (GenreName),
+    FOREIGN KEY (SongID)
+        REFERENCES Song (SongID)
 );
 
-CREATE TABLE Artist
-(
-  ArtistID INT NOT NULL AUTO_INCREMENT,
-  StageName VARCHAR(50) NOT NULL,
-  SoloFlag Boolean NOT NULL,
-  ArtistName VARCHAR(50),
-  DateOfBirth DATE,
-  BandFlag Boolean NOT NULL,
-  Established DATE,
-  PRIMARY KEY (ArtistID)
+CREATE TABLE Artist (
+    ArtistID INT NOT NULL AUTO_INCREMENT,
+    StageName VARCHAR(50) NOT NULL,
+    SoloFlag BOOLEAN NOT NULL,
+    ArtistName VARCHAR(50),
+    DateOfBirth DATE,
+    BandFlag BOOLEAN NOT NULL,
+    Established DATE,
+    PRIMARY KEY (ArtistID)
 );
 
-CREATE TABLE MemberOf
-(
-  GroupID INT NOT NULL,
-  IndividualID INT NOT NULL,
-  PRIMARY KEY (GroupID, IndividualID),
-  FOREIGN KEY (GroupID) REFERENCES Artist(ArtistID),
-  FOREIGN KEY (IndividualID) REFERENCES Artist(ArtistID)
+CREATE TABLE MemberOf (
+    GroupID INT NOT NULL,
+    IndividualID INT NOT NULL,
+    PRIMARY KEY (GroupID , IndividualID),
+    FOREIGN KEY (GroupID)
+        REFERENCES Artist (ArtistID),
+    FOREIGN KEY (IndividualID)
+        REFERENCES Artist (ArtistID)
 );
 
 CREATE TABLE Inventory (
@@ -83,11 +82,12 @@ CREATE TABLE Inventory (
         REFERENCES Location (LocationID)
 );
 
-CREATE TABLE SongFeatureList
-(
-  SongID INT NOT NULL,
-  ArtistID INT NOT NULL,
-  PRIMARY KEY (SongID, ArtistID),
-  FOREIGN KEY (SongID) REFERENCES Song(SongID),
-  FOREIGN KEY (ArtistID) REFERENCES Artist(ArtistID)
+CREATE TABLE SongFeatureList (
+    SongID INT NOT NULL,
+    ArtistID INT NOT NULL,
+    PRIMARY KEY (SongID , ArtistID),
+    FOREIGN KEY (SongID)
+        REFERENCES Song (SongID),
+    FOREIGN KEY (ArtistID)
+        REFERENCES Artist (ArtistID)
 );
