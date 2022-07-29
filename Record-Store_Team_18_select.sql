4001d9bd9766f4259460fe2990f5ac6015d2fd18
sstabile2U <93227889+sstabile2U@users.noreply.github.com>
2022-07-29 10:24:32 -0400

    added query 2

--- Record-Store_Team_18_select.sql
+++ Record-Store_Team_18_select.sql
@@ -140,4 +140,18 @@ WHERE
     Album.ReleaseDate BETWEEN '1970-01-01' AND '2016-01-01'
 ORDER BY Album.ReleaseDate;
 
-
+-- Find all albums that have songs of a particular genre type
+SELECT DISTINCT
+	Genre.GenreName as 'Genre',
+	AlbumTitle as 'Album'
+FROM 
+	Album, Genre, Song, SongGenreList
+WHERE
+	Album.AlbumID = Song.AlbumID
+AND
+	Song.SongID = SongGenreList.SongID
+AND
+	SongGenreList.GenreName = Genre.GenreName
+AND
+	Genre.GenreName = 'Pop';
+    
