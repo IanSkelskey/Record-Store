ab2e982978815924216bd374bd04bf046a699f19
Nathanael Swecker <nswecker@asu.edu>
2022-08-02 11:36:16 -0700

    Added json dependency

--- src/main/java/QueryLogic.java
+++ src/main/java/QueryLogic.java
@@ -2,23 +2,20 @@
 * QueryLogic
 */
 import java.sql.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.json.*;
+
+import util.DBConnection;
+
+import java.awt.List;
 import java.io.*;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.*;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
-
-
 public class QueryLogic{
-	
-	private Connection conn;
+    
+    private PreparedStatement pStatement;
+    private ResultSet resultSet;
 	
 	public QueryLogic(){
 		
@@ -26,6 +23,58 @@ public class QueryLogic{
 	
 	public void getAlbumStockAtAllLocations(String albumTitle){
 		System.out.println("Thinking this will print a list of availability by location");
+		
+		closeAllResources();
+	}
+	
+	public ResultSet listAllEmployeesEverywhere() {
+	    return resultSet;
+	}
+	
+	public JSONArray toJSON() {
+	    JSONArray result = null;
+	    try {
+	        ResultSetMetaData md = resultSet.getMetaData();
+	        int numCols = md.getColumnCount();
+	        java.util.List<String> colNames = IntStream.range(0, numCols)
+	          .mapToObj(i -> {
+	              try {
+	                  return md.getColumnName(i + 1);
+	              } catch (SQLException e) {
+	                  e.printStackTrace();
+	                  return "?";
+	              }
+	          })
+	          .collect(Collectors.toList());
+
+	        result = new JSONArray();
+	        while (resultSet.next()) {
+	            JSONObject row = new JSONObject();
+	            colNames.forEach(cn -> {
+	                try {
+	                    row.put(cn, resultSet.getObject(cn));
+	                } catch (JSONException | SQLException e) {
+	                    e.printStackTrace();
+	                }
+	            });
+	            result.put(row);
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+
+	    
+	    return result;
+	}
+	
+	private void closeAllResources() {
+        try {
+            if (resultSet != null) resultSet.close();
+            if (pStatement != null) pStatement.close();
+            if (DBConnection.getInstance().getConnection() != null) DBConnection.getInstance().closeConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 	}
 	
 }
