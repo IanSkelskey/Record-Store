6eec85df52413f0703d8b90dc431514c5130ffc4
Nathanael Swecker <nswecker@asu.edu>
2022-08-02 18:17:55 -0700

    removed old singleton code from querylogic

--- src/main/java/QueryLogic.java
+++ src/main/java/QueryLogic.java
@@ -16,8 +16,6 @@ import java.io.*;
 
 public class QueryLogic{
     
-    private static final QueryLogic qLogic = new QueryLogic();
-    
     private static PreparedStatement pStatement;
     private static ResultSet resultSet;
     private static JSONArray jsonResult;
@@ -46,42 +44,6 @@ public class QueryLogic{
        
        return jsonResult;
    }
-	
-	// FROM: https://www.baeldung.com/java-jdbc-convert-resultset-to-json
-	public static JSONArray toJSON(ResultSet resultSet) {
-	    JSONArray jsonResult = null;
-	    try {
-	        ResultSetMetaData md = resultSet.getMetaData();
-	        int numCols = md.getColumnCount();
-	        java.util.List<String> colNames = IntStream.range(0, numCols)
-	          .mapToObj(i -> {
-	              try {
-	                  return md.getColumnName(i + 1);
-	              } catch (SQLException e) {
-	                  e.printStackTrace();
-	                  return "?";
-	              }
-	          })
-	          .collect(Collectors.toList());
-
-	        jsonResult = new JSONArray();
-	        while (resultSet.next()) {
-	            JSONObject row = new JSONObject();
-	            colNames.forEach(cn -> {
-	                try {
-	                    row.put(cn, resultSet.getObject(cn));
-	                } catch (JSONException | SQLException e) {
-	                    e.printStackTrace();
-	                }
-	            });
-	            jsonResult.put(row);
-	        }
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    }
-
-	    return jsonResult;
-	}
 	
 	public static JSONArray altJSON(ResultSet rs) {
 	    JSONArray json = new JSONArray();
