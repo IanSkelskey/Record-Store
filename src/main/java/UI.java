bedbcd8818d33a37f2a2378ce7af9992dea6f3a7
Andrew Tonn <attonn@asu.edu>
2022-08-04 19:20:07 -0400

    Removed insert test in UI

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -29,32 +29,7 @@ public class UI{
 	}
 	
 	public static void main(String[] args){
-		DBConnection dbCon = DBConnection.getInstance();
-		
-		if (args.length != 3) {
-			System.out.println("Incorrect number of arguments. " +
-					"Please provide url, user, password, and driver to access database." +
-					"Exiting now. Please try again.");
-			System.exit(1);
-		}
-
-		String url = args[0];
-		String user = args[1];
-		String pwd = args[2];
-
-		dbCon.setConnection(url, user, pwd);
 		System.out.println("Welcome...");
 		
 		displayMenu();
-		
-		//Just left this to test the insert method. Will be going before merged into development
-		HashMap<String, String> hashInsert = new HashMap<String, String>();
-		hashInsert.put("stagename", "\"Ted Nugent\"");
-		hashInsert.put("soloflag", "true");
-		hashInsert.put("artistname", "\"Ted Nugent\"");
-		hashInsert.put("dateofbirth", "\"1945-01-02\"");
-		hashInsert.put("bandflag", "false");
-		hashInsert.put("established", "\"1945-01-02\"");
-		instance.updater.insert("artist", hashInsert);
-	}
 }
