b0de009a98f16eccf7bbe82faacd94a603e75519
Ian Skelskey <ianskelskey@gmail.com>
2022-08-09 21:38:05 -0400

    Finish Update Location dialog

--- src/main/java/gui/controllers/UpdateLocationDialogController.java
+++ src/main/java/gui/controllers/UpdateLocationDialogController.java
@@ -6,41 +6,51 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
-import org.json.JSONArray;
-import util.Query;
+import util.AttributeGetter;
 import util.SQLHelper;
 
-import java.util.ArrayList;
+import java.util.Map;
 
 public class UpdateLocationDialogController {
     @FXML
     private DialogPane pane;
 
     @FXML
-    private ChoiceBox<String> employeeChoice;
+    private ChoiceBox<String> locationChoice;
 
     @FXML
     private ChoiceBox<String> attributesChoice;
 
+    @FXML
+    private TextField newValueField;
+
     public void initialize() {
+        populateLocationChoice();
         populateAttributesChoice();
         Button okButton = (Button) pane.lookupButton(ButtonType.OK);
         okButton.addEventFilter(ActionEvent.ACTION, event -> handleOKButton());
     }
 
     private void handleOKButton () {
-
+        String locationID = AttributeGetter.getLocationIDFromName(locationChoice.getValue());
+        SQLHelper.update("Location", attributesChoice.getValue(),
+                newValueField.getText(), "LocationID", locationID);
     }
 
-    public void populateAttributesChoice() {
+    private void populateAttributesChoice() {
         ObservableList<String> attributes = FXCollections.observableArrayList();
-        attributes.addAll(getAllModifiableEmployeeAttributes());
+        attributes.addAll(AttributeGetter.getAllModifiableEmployeeAttributes());
         this.attributesChoice.setItems(attributes);
+        this.attributesChoice.setValue(attributes.get(0));
     }
 
-    private ArrayList<String> getAllModifiableEmployeeAttributes() {
-        JSONArray results = SQLHelper.getQueryResultsAsJSON(Query.EMPLOYEES_INFO.query, new ArrayList<>());
-        return new ArrayList<>(results.getJSONObject(0).keySet());
+    private void populateLocationChoice() {
+        ObservableList<String> locations = FXCollections.observableArrayList();
+        for (Map.Entry<Integer, String> entry: AttributeGetter.getAllLocationIDsAndNames().entrySet()) {
+            locations.add(entry.getValue());
+        }
+        locationChoice.setItems(locations);
+        locationChoice.setValue(locations.get(0));
     }
 
 }
